617c9db839d97aa2da259f2303264eef507ef916
akdev5 <akio.mori555@gmail.com>
2020-06-17 10:45:31 +0300

    add api structure

--- puppy_store/puppy_store/urls.py
+++ puppy_store/puppy_store/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import include, path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -32,5 +33,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     re_path(r'^(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    path('api/v1/', include('puppies.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
     path('admin/', admin.site.urls),
 ]
